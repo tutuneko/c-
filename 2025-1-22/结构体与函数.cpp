b7f5e9998ff46424318ac7bb4ca442abc84cc235
tutuneko <BF2067256514@outlook.com>
2025-01-23 17:39:26 +0800

    Add files via upload

--- 2025-1-22/结构体与函数.cpp
+++ 2025-1-22/结构体与函数.cpp
@@ -2,26 +2,37 @@
 using namespace std;
 #include<string>
 //2025年1月22日 19:04:20
-struct t {
+struct A {
 	string name;
 	int age;
 
 };
-void a11(t a1){
-	cout << a1.age << endl << a1.name << endl;
+void S(A AL) {
+	AL.age = AL.age + 100;
+	cout << "姓名：" << AL.name << "年龄：" << AL.age << endl;
 }
-int main() {
-	t a1 = { "A",18 };
-	a11(a1);
+void SS(A* AL) {
+	AL->age = AL->age + 100;
+	cout << "姓名：" << AL->name << "年龄：" << AL->age << endl;
+}
+int main(){
+	A AL = { "AA",18 };
+	S(AL);
+	cout << "值传递：\t" << AL.name << "\t" << AL.age << endl;
+	SS(&AL);
+	cout << "地址传递：\t" << AL.name << "\t" << AL.age << endl;
 	system("pause");
 	return 0;
 }
 /*
 界面——————————————
-18
-A
+姓名：AA年龄：118
+值传递：        AA      18
+姓名：AA年龄：118
+地址传递：      AA      118
 请按任意键继续. . .
 功能——————————————
+通过地址传递与值传递打印结构体
 总结——————————————
-1.命名变量等，不能用数字开头
+值传递占用内存比地址传递大，但不修改值，地址占用内存小，但形参影响实参
 */
